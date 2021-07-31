80baba500f28ed27a46172c2c78348fae50be80d
komeil13 <komeili.hadi@gmail.com>
2021-07-31 21:31:06 +0430

    add url pattern and app name for landing - add test for base model

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class MyTestModelTest(TestCase):
+
+    def setUp(self) -> None:
+        self.ins1 = TestModel.objects.create()
+
+
+    def test_create_instance(self):
+        self.ins1.save()
+
+        self.assertNotIn(self.ins1, TestModel.objects.all())
+
+
+    def test_instance_deleted(self):
+        self.ins1.deleted = True
+        self.ins1.save()
+
+        self.assertIn(self.ins1, TestModel.objects.all())
+
+    def test_instance_in_archive(self):
+        self.ins1.deleted = True
+        self.ins1.save()
+
+        self.assertNotIn(self.ins1, TestModel.objects.archive())
+
+    def test2_instance_in_archive(self):
+        self.ins1.save()
+
+        self.assertNotIn(self.ins1, TestModel.objects.archive())
